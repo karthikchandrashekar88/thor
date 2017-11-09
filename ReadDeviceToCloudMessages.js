de4a8446f6a6f5ca82188dd08f904da6cd9c8bc4
karthik chandrashekar <karthik.c@lnttechservices.com>
2017-11-09 23:22:47 +0530

    ---

--- ReadDeviceToCloudMessages.js
+++ ReadDeviceToCloudMessages.js
@@ -1,23 +1,26 @@
 'use strict';
 
 var EventHubClient = require('azure-event-hubs').Client;
+//HostName=ThorIot.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=0jUWAQSG3NX6riDlWdmajdYHrg1H9jjLOBWMGkAgr34=
+//var connectionString = 'HostName=ThorIot.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=0jUWAQSG3NX6riDlWdmajdYHrg1H9jjLOBWMGkAgr34=';
 var connectionString = 'HostName=ThorIot.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=0jUWAQSG3NX6riDlWdmajdYHrg1H9jjLOBWMGkAgr34=';
-var printError = function (err) {
-  console.log(err.message);
+var printError = function(err) {
+    console.log(err.message);
 };
 
-var printMessage = function (message) {
-  console.log('Message received: ');
-  console.log(JSON.stringify(message.body));
-  console.log('');
+var printMessage = function(message) {
+    console.log('Message received: ');
+    console.log(JSON.stringify(message.body));
+    console.log('');
 };
 
 var client = EventHubClient.fromConnectionString(connectionString);
 client.open()
     .then(client.getPartitionIds.bind(client))
-    .then(function (partitionIds) {
-        return partitionIds.map(function (partitionId) {
-            return client.createReceiver('$Default', partitionId, { 'startAfterTime' : Date.now()}).then(function(receiver) {
+    .then(function(partitionIds) {
+        return partitionIds.map(function(partitionId) {
+            console.log('partitionId :', partitionId);
+            return client.createReceiver('$Default', partitionId, { 'startAfterTime': Date.now() }).then(function(receiver) {
                 console.log('Created partition receiver: ' + partitionId)
                 receiver.on('errorReceived', printError);
                 receiver.on('message', printMessage);
--- SimulatedDevice.js
+++ SimulatedDevice.js
@@ -1,36 +1,31 @@
-
-
 var clientFromConnectionString = require('azure-iot-device-mqtt').clientFromConnectionString;
 var Message = require('azure-iot-device').Message;
 
-var connectionString = 'HostName=ThorIot.azure-devices.net;DeviceId=myFirstNodeDevice;SharedAccessKey=BFA3TlqpDOqgXoiv7sL9PNAaOrfqrD5L7Zktc6jHcuc=';
-
+//var connectionString = 'HostName=ThorIot.azure-devices.net;DeviceId=myFirstNodeDevice;SharedAccessKey=BFA3TlqpDOqgXoiv7sL9PNAaOrfqrD5L7Zktc6jHcuc=';
+var connectionString = 'HostName=ThorIot.azure-devices.net;DeviceId=OnlineWebClient;SharedAccessKey=blA5Xz24D2LVk7AJhhxCoKer1Fgq0Ng1fE3B9s8PpZ0=';
 var client = clientFromConnectionString(connectionString);
 
 function printResultFor(op) {
-  return function printResult(err, res) {
-    if (err) console.log(op + ' error: ' + err.toString());
-    if (res) console.log(op + ' status: ' + res.constructor.name);
-  };
+    return function printResult(err, res) {
+        if (err) console.log(op + ' error: ' + err.toString());
+        if (res) console.log(op + ' status: ' + res.constructor.name);
+    };
 }
 
-var connectCallback = function (err) {
-  if (err) {
-    console.log('Could not connect: ' + err);
-  } else {
-    console.log('Client connected');
-
-    // Create a message and send it to the IoT Hub every second
-    setInterval(function(){
+var connectCallback = function(err) {
+    if (err) {
+        console.log('Could not connect: ' + err);
+    } else {
+        console.log('Client connected');
+        // Create a message and send it to the IoT Hub every second
         var temperature = 20 + (Math.random() * 15);
         var humidity = 60 + (Math.random() * 20);
-        var data = JSON.stringify({ deviceId: 'myFirstNodeDevice', temperature: temperature, humidity: humidity });
+        var data = JSON.stringify({ deviceId: 'CARS', temperature: temperature, humidity: humidity });
         var message = new Message(data);
         message.properties.add('temperatureAlert', (temperature > 30) ? 'true' : 'false');
         console.log("Sending message: " + message.getData());
         client.sendEvent(message, printResultFor('send'));
-    }, 1000);
-  }
+    }
 };
 
 client.open(connectCallback);
